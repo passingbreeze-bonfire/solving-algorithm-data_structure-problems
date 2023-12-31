d0ff813840d4e1036292bff6903f902cfca11ad5
passingbreeze <jimmy@triple-corp.com>
2023-12-31 16:48:47 +0900

    fix: problem 1897 first solution

--- prob1897/solution/solution.go
+++ prob1897/solution/solution.go
@@ -1,5 +1,18 @@
 package solution
 
 func MakeEqual(words []string) bool {
-	return false
+	answer := make(map[string]int)
+	for _, word := range words {
+		for _, char := range word {
+			answer[string(char)]++
+		}
+	}
+	var prev int = 0
+	for _, count := range answer {
+		if prev != 0 && prev != count {
+			return false
+		}
+		prev = count
+	}
+	return true
 }
--- prob1897/solution/solution_test.go
+++ prob1897/solution/solution_test.go
@@ -8,6 +8,11 @@ import (
 func TestMakeEqual(t *testing.T) {
 	case1 := []string{"abc", "aabc", "bc"}
 	if !solution.MakeEqual(case1) {
-		t.Error("Expected true, got false")
+		t.Error("Case1 Expected true, got false")
+	}
+
+	case2 := []string{"ab", "a"}
+	if solution.MakeEqual(case2) {
+		t.Error("Case2 Expected false, got true")
 	}
 }
