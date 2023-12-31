1cd2505332e44e514233c7693dfefdf2f85faad0
passingbreeze <jimmy@triple-corp.com>
2023-12-31 18:40:30 +0900

    fix: solv problems

--- prob1897/solution/solution.go
+++ prob1897/solution/solution.go
@@ -1,21 +1,19 @@
 package solution
 
 func MakeEqual(words []string) bool {
-    answer := make(map[string]int)
+	freqLen := len(words) // 반복되는 글자의 수는 words의 길이와 같다.
+	answer := make(map[string]int)
+
 	for _, word := range words {
 		for _, char := range word {
 			answer[string(char)]++
 		}
 	}
-	var prev int = 0
+
 	for _, count := range answer {
-		if prev != 0 && prev != count {
+		if count % freqLen != 0 {
 			return false
 		}
-		prev = count
 	}
-    if len(answer) > 0 && prev == 1 {
-        return false
-    }
 	return true
 }
