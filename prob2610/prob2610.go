a116cc912c95b430056da7f49794bac86c96afdd
passingbreeze <jungmin1237@pusan.ac.kr>
2024-01-03 03:32:51 +0900

    feat: add solution

--- prob2610/prob2610.go
+++ prob2610/prob2610.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"solving-algorithm-data_structure-problems/prob2610/solution"
 )
 
 func main() {
 	fmt.Println("Here is problem 2610 solution.")
+	nums1 := []int{1, 2, 3, 4}
+	fmt.Println(solution.FindMatrix(nums1))
+
+	nums2 := []int{1, 3, 4, 1, 2, 3, 1}
+	fmt.Println(solution.FindMatrix(nums2))
 }
