bec2fecd848adae5168926bd96efa1370b7f7881
passingbreeze <jeongmin1237@interparktriple.com>
2024-04-15 23:40:12 +0900

    feat: prob1624 solved

---
+++ prob1624/go.mod
@@ -0,0 +1,3 @@
+module solving-algorithm-data_structure-problems/prob1624
+
+go 1.21.5
---
+++ prob1624/solution/solution_test.go
@@ -0,0 +1,18 @@
+package solution_test
+
+import (
+	"solution"
+	"testing"
+)
+
+func TestMakeEqual(t *testing.T) {
+	case1 := []string{"abc", "aabc", "bc"}
+	if !solution.MakeEqual(case1) {
+		t.Error("Case1 Expected true, got false")
+	}
+
+	case2 := []string{"ab", "a"}
+	if solution.MakeEqual(case2) {
+		t.Error("Case2 Expected false, got true")
+	}
+}
